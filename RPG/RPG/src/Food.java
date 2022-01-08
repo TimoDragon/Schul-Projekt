8ccb6d9dcfff7416f9514382276ae0e5e9a1ea1b
BuildTools <unconfigured@null.spigotmc.org>
2022-01-08 18:21:40 +0100

    added Food

---
+++ RPG/RPG/src/Food.java
@@ -0,0 +1,36 @@
+public class Food extends Item {
+    private int saturation = 0;
+    private boolean canHeal = false;
+    private int healingFactor = 0;
+
+    public Food() {
+    }
+    public Food(int saturation) {
+        this.saturation = saturation;
+    }
+
+    //get the Food saturation
+    public int getSaturation() {
+        return saturation;
+    }
+
+    //set the Food saturation
+    public void setSaturation(int saturation) {
+        this.saturation = saturation;
+    }
+
+    //check if the food can heal
+    public boolean canHeal() {
+        return canHeal;
+    }
+
+    //set the healing factor of the food
+    public void setHealingFactor(int healingFactor) {
+        this.healingFactor = healingFactor;
+    }
+
+    //get the healing factor of the food
+    public int getHealingFactor() {
+        return healingFactor;
+    }
+}
