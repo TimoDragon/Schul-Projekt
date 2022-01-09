8d85d29379e6a0fd73104a20cc1a9f27b7efc406
BuildTools <unconfigured@null.spigotmc.org>
2022-01-09 17:35:51 +0100

    added constructor

--- RPG/RPG/src/Weapon.java
+++ RPG/RPG/src/Weapon.java
@@ -2,9 +2,18 @@ public class Weapon extends Item {
     private String name;
     private int damage;
     
+    public Weapon(String name, String type, int damage) {
+        this.name = name;
+        this.setType(type);
+        this.damage = damage;
+    }
+
+    public Weapon() {
+    }
+
     //sets the weapon damage
-    public void setDamage(int i) {
-        damage = i;
+    public void setDamage(int damage) {
+        this.damage = damage;
     }
 
     //get the weapon damage
@@ -13,8 +22,8 @@ public class Weapon extends Item {
     }
 
     //sets the Name
-    public void setName(String input) {
-        name = input;
+    public void setName(String name) {
+        this.name = name;
     }
 
     //get the name
