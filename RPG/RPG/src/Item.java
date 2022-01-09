887d8b92bcd9a7b77ba19d42fdbef6d48ed52043
BuildTools <unconfigured@null.spigotmc.org>
2022-01-09 17:34:59 +0100

    added constructor and price

--- RPG/RPG/src/Item.java
+++ RPG/RPG/src/Item.java
@@ -1,9 +1,27 @@
 public class Item {
     private String type;
+    private int price;
+
+    public Item(String type) {
+        this.type = type;
+    }
+
+    public Item() {
+    }
+
+    //set the Item price for the shop
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    //get the item price
+    public int getPrice() {
+        return price;
+    }
 
     //set the Item Type
-    public void setType(String string) {
-        type = string;
+    public void setType(String type) {
+        this.type = type;
     }
 
     //get the Item Type
@@ -13,7 +31,6 @@ public class Item {
 
     public Weapon toWeapon() {
         Weapon weapon = (Weapon) this;
-        
         return weapon;
     }
 }
